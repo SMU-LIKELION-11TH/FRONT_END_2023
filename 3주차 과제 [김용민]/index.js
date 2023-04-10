8ff00738a42a53ca8916ae7882aa14e038898092
김용민 <dydals3440@gmail.com>
2023-04-10 15:10:06 +0900

    [3주차 과제] 김용민

--- 3주차 과제 [김용민]/index.js
+++ 3주차 과제 [김용민]/index.js
@@ -1,4 +1,6 @@
-const text = '안녕하세요✋ 개발공부하는 경영학부 김용민입니다.';
+// text Write Effect
+
+const text = '안녕하세요✋ 개발하는 경영학부 김용민입니다.';
 const textEl = document.querySelector('.text');
 
 let idx = 1;
@@ -15,4 +17,25 @@ function writeText() {
   }
 
   setTimeout(writeText, speed);
+}
+
+// Loading Blur Effect
+const container = document.querySelector('.container');
+const blurText = document.querySelector('.blurText');
+
+let percentage = 0;
+let int = setInterval(blurring, 30);
+
+blurring();
+
+function blurring() {
+  percentage++;
+  blurText.innerHTML = `${percentage}%`;
+  console.log(percentage);
+  if (percentage > 100) {
+    clearInterval(int);
+    // Loading Percantage 100될떄 삭제
+    blurText.style.display = `none`;
+  }
+  container.style.filter = `blur(${100 - percentage}px)`;
 }
