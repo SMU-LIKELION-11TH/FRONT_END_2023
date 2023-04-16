c1bf4cfc883f23c3b42615fe3dc95bbbe6b67281
woogi123 <63836914+woogi123@users.noreply.github.com>
2023-04-16 15:40:12 +0900

    Update main.js

---
+++ 3주차-세미나/3주차세미나배포용/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $(".logo_btn").click(function(){
+        $(".narbar_menu").toggleClass("active");
+        $(".narbar_links").toggleClass("active");
+        
+    });
+});
