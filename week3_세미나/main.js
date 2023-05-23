d006423015234040616b2a82d380fad526beaf4f
seokhyun Yoon <69806024+master1st@users.noreply.github.com>
2023-05-23 22:53:05 +0900

    Revert "이수정(5주차 과제)"

--- week3_세미나/main.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $(".logo_btn").click(function () {
-        $(".navbar_menu").toggleClass("active");
-        $(".navbar_links").toggleClass("active");
-    });
-});
