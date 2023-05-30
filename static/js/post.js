7b18a9dd6dfdce7f4186ab32b892ec8be25a75dd
HYERINI <rinlee2001@naver.com>
2023-05-30 16:51:00 +0900

    [ADD] logout

--- static/js/post.js
+++ static/js/post.js
@@ -25,3 +25,17 @@ function moveDetailPost(id){
 function movePost(){
     window.location = '/post/create/';
 }
+
+function logout(){
+    $.ajax({
+        type: 'GET',
+        url: 'http://127.0.0.1:8000/account/user/logout/',
+        contentType: 'application/json',
+        success: function(data){
+            alert('로그아웃 성공!!');
+            window.location = '/post/';
+        }, error: function(){
+            alert('로그아웃을 할 수 없네요 !!');
+        }
+    })
+}
