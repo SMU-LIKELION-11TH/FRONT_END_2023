8272a92911c1b75fbb2ad57fd03c17f77b09c74d
HYERINI <rinlee2001@naver.com>
2023-05-31 18:18:10 +0900

    [EDIT]logout

--- static/js/logout.js
+++ static/js/logout.js
@@ -1,13 +1,3 @@
 function logout(){
-    $.ajax({
-        type: 'GET',
-        url: 'http://127.0.0.1:8000/account/user/logout/',
-        contentType: 'application/json',
-        success: function(data){
-            alert('로그아웃 성공!!');
-            window.location = '/';
-        }, error: function(){
-            alert('로그아웃을 할 수 없네요 !!');
-        }
-    })
+
 }
