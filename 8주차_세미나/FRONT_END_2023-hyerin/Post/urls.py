eb16117371228d2a4741767ea5fcb4307b808043
Whatdoyumin <lym4963@naver.com>
2023-05-31 19:52:12 +0900

    6주차/7주차_세미나(이유민)

---
+++ 8주차_세미나/FRONT_END_2023-hyerin/Post/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+app_name = "post"
+
+urlpatterns = [
+    path('create/', views.PostCreate.as_view(),name = "create"),
+    path('list/cbv/', views.PostListView.as_view(),name = "list"),
+    path('list/', views.PostListApiView.as_view(),name = "list_api"),
+    path('detail/<int:pk>/cbv', views.PostDetailView.as_view(),name = "detail"),
+    path('detail/<int:pk>/', views.PostDetailApiView.as_view(),name = "detail_api"),
+    path('update/<int:pk>/', views.PostUpdateView.as_view(),name = "update"),
+    path('delete/<int:pk>/', views.PostDeleteView.as_view(),name = "delete"),
+
+]
+
+
