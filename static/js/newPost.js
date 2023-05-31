24d30fae3f77704b722fb314097183e575a2ac9e
HYERINI <rinlee2001@naver.com>
2023-05-31 17:57:12 +0900

    [DELETE]newPost.js

--- static/js/newPost.js
+++ static/js/newPost.js
@@ -1,17 +1,12 @@
 function completeNewPost(){
     $.ajax({
-        type: 'PUT',
-        url: 'http://127.0.0.1:8000/post/create/',
+        type: '',
+        url: '',
         contentType: 'application/json',
         data: JSON.stringify({
-            'title' : $('#postTitle').val(),
-            'content' : $('#postContent').val(),
         }),
         success: function(data){
-            alert('글 등록을 완료했습니다.');
-            window.location = './post.html';
         }, error: function(){
-            alert('글 등록을 실패했습니다!!!!');
         }
     })
 }
